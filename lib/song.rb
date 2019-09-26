98d0f64e5e7eb2b1c69fc251c8a73db013abb2ff
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-09-26 23:11:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,16 +3,21 @@ class Song
 attr_accessor :name, :artist, :genre
 
 @@count = 0
+@@genres = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@genres << genre
     @@count += 1
   end
   
   def count
     @@count
   end
+  
+  def genres(genre)
+    
   
 end
