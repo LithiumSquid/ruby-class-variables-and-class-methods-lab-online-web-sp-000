4a209d1a995e6a55bc6f7e3ca5610a352c939acd
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-09-26 23:16:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,12 +4,14 @@ attr_accessor :name, :artist, :genre
 
 @@count = 0
 @@genres = []
+@@artists = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@genres << genre
+    
     @@count += 1
   end
   
@@ -19,4 +21,8 @@ attr_accessor :name, :artist, :genre
   
   def genres(genre)
     @@genres.uniq!
+  end
+  
+  def artists
+    @@artists.uniq!
 end
